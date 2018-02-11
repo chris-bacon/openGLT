b15114074be1519203037a4754399f8e6406bd06
Chris Bacon <chrisbacon2009@hotmail.com>
2018-02-11 18:04:04 +0000

    Added comments describing OpenGL functions

--- src/Shaders/Shader.hs
+++ src/Shaders/Shader.hs
@@ -3,15 +3,19 @@ module Shaders.Shader where
 import qualified Graphics.Rendering.OpenGL as OpenGL
 import qualified Graphics.UI.GLUT as Glut
 
---OpenGL.createShader
---OpenGL.createProgram
-
 shaderTypes :: [OpenGL.ShaderType]
 shaderTypes = [OpenGL.VertexShader, OpenGL.FragmentShader]
 
+-- glCreateProgram creates an empty program object and returns a non-zero value by which it can be referenced. A program object 
+-- is an object to which shader objects can be attached. This provides a mechanism to specify the shader objects that will be 
+-- linked to create a program
 createProgram :: IO OpenGL.Program
 createProgram = OpenGL.createProgram
 
+-- glCreateShader creates an empty shader object and returns a non-zero value by which it can be referenced. 
+-- glShaderSource sets the source code in shader to the source code in the array of strings specified by string. 
+-- Any source code previously stored in the shader object is completely replaced.
+-- glCompileShader compiles the source code strings that have been stored in the shader object.
 --createShader :: OpenGL.ShaderType -> String -> IO Shader
 createShader shaderType shaderText = do
     shader <- OpenGL.createShader shaderType
@@ -19,8 +23,14 @@ createShader shaderType shaderText = do
     OpenGL.compileShader shader -- glCompileShader
     return shader
 
+-- In order to create a complete shader program, there must be a way to specify the list of things that will be linked together. 
+-- Program objects provide this mechanism. Shaders that are to be linked together in a program object must first be attached to
+-- that program object. glAttachShader attaches the shader object specified by shader to the program object specified by program. 
+-- This indicates that shader will be included in link operations that will be performed on program.
 attachShader :: OpenGL.Program -> OpenGL.Shader -> IO ()
 attachShader p s = OpenGL.attachShader p s
+
+
 
 --attribLocation :: OpenGL.Program -> String
 attribLocation program string = OpenGL.attribLocation program string $= OpenGL.AttribLocation 0
